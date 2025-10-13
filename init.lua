ae2cb32e85daf3f9a40a9170d633f8b1747b7833
CaesarLi-WantToBeAFreeMan <krbw0110010000@gmail.com>
2025-10-13 14:20:25 +0800

    add key mappings

--- init.lua
+++ init.lua
@@ -3,3 +3,4 @@ vim.g.mapleader = "\\"          --global leader key
 vim.g.maplocalleader = "\\"     --local leader key
 
 require("config.options")       --load options
+require("config.keymaps")       --load keymaps
