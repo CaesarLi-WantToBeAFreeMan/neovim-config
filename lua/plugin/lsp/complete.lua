90827e0244c4cc1394cd6491677d2c69af083d97
CaesarLi-WantToBeAFreeMan <krbw0110010000@gmail.com>
2025-11-07 22:42:11 +0800

    beatify completion UI

--- lua/plugin/lsp/complete.lua
+++ lua/plugin/lsp/complete.lua
@@ -4,14 +4,16 @@ return{
     dependencies = {
         "hrsh7th/cmp-buffer",                       --buffer text source
         "hrsh7th/cmp-path",                         --file system path source
+        "hrsh7th/cmp-nvim-lsp",
+        "saadparwaiz1/cmp_luasnip",                 --lua snippt
+        "onsails/lspkind.nvim",                     --Microsoft Visual Studio Code like pictograms
         {
             "L3MON4D3/LuaSnip",                     --define, expand and jump through code snippets
             version = "v2.*",
             build = "make install_jsregexp"
         },
-        "saadparwaiz1/cmp_luasnip",                 --lua snippt
-        "rafamadriz/friendly-snippets",             --useful snippets
-        "onsails/lspkind.nvim"                      --Microsoft Visual Studio Code like pictograms
+        "lukas-reineke/cmp-under-comparator",
+        "rafamadriz/friendly-snippets"              --useful snippets
     },
     config = function()
         local cmp, luasnip, lspkind = require("cmp"),  require("luasnip"), require("lspkind")
@@ -20,36 +22,94 @@ return{
         require("luasnip.loaders.from_vscode").lazy_load()
 
         cmp.setup({
-            snippet = {                                                 --how nvim-cmp interacts with snippet engine
+            snippet = {
                 expand = function(args)
                     luasnip.lsp_expand(args.body)
                 end
             },
+
             mapping = cmp.mapping.preset.insert({
-                ["<C-i>"] = cmp.mapping.complete(),                     --trigger completion
-                ["<CR>"] = cmp.mapping.confirm({select = true}),        --confirm selection
-                ["<C-e>"] = cmp.mapping.abort(),                        --close completion window
-                ["<C-j>"] = cmp.mapping.select_next_item(),             --select next item
-                ["<C-n>"] = cmp.mapping.select_next_item(),
-                ["<C-k>"] = cmp.mapping.select_prev_item(),             --select previous item
-                ["<C-p>"] = cmp.mapping.select_prev_item(),
-                ["<C-f>"] = cmp.mapping.scroll_docs(10),                --scroll up 10 items
-                ["<C-b>"] = cmp.mapping.scroll_docs(-10),               --scroll down 10 items
-                ["<C-d>"] = cmp.mapping.scroll_docs(5),                 --scroll up 10 items
-                ["<C-u>"] = cmp.mapping.scroll_docs(-5)                 --scroll down 10 items
+                ["<C-i>"]   = cmp.mapping.complete(),                   --trigger completion
+                ["<CR>"]    = cmp.mapping.confirm({select = true}),     --confirm selection
+                ["<C-e>"]   = cmp.mapping.abort(),                      --close completion window
+                ["<C-j>"]   = cmp.mapping.select_next_item(),           --select next item
+                ["<C-n>"]   = cmp.mapping.select_next_item(),
+                ["<C-k>"]   = cmp.mapping.select_prev_item(),           --select previous item
+                ["<C-p>"]   = cmp.mapping.select_prev_item(),
+                ["<C-f>"]   = cmp.mapping.scroll_docs(10),              --scroll up 10 items
+                ["<C-b>"]   = cmp.mapping.scroll_docs(-10),             --scroll down 10 items
+                ["<C-d>"]   = cmp.mapping.scroll_docs(5),               --scroll up 5 items
+                ["<C-u>"]   = cmp.mapping.scroll_docs(-5)               --scroll down 5 items
             }),
+
             sources = cmp.config.sources({
                 {name = "nvim_lsp"},                                    --LSP symbols
                 {name = "luasnip"},                                     --snippets
                 {name = "buffer"},                                      --text within current buffer
                 {name = "path"}                                         --file system path
             }),
+
             formatting = {
                 format = lspkind.cmp_format({
+                    mode = "text",
                     maxwidth = 50,
-                    ellipsts_char = "..."
+                    ellipsis_char = "󰟃",
+                    symbol_map = {
+                        Variable        = "󰫧",
+                        Field           = "󰬅",
+                        Function        = "󰡱",
+                        Method          = "󰊕",
+                        Constructor     = "󰩀",
+                        Class           = "󰬊",
+                        Enum            = "󱡠",
+                        Interface       = "󰬐",
+                        Struct          = "󰬚",
+                        Module          = "󰏓",
+                        Namespace       = "󰬕",
+                        Package         = "",
+                        Property        = "󰬗",
+                        Text            = "",
+                        Unit            = "",
+                        Value           = "󰎠",
+                        Keyword         = "",
+                        Snippet         = "",
+                        Color           = "󰏘",
+                        File            = "",
+                        Reference       = "",
+                        Folder          = "",
+                        EnumMember      = "",
+                        Constant        = "󰐀",
+                        Event           = "",
+                        Operator        = "",
+                        TypeParameter   = ""
+                    },
+                    menu = {
+                        buffer = "",
+                        nvim_lsp = "",
+                        luasnip = "",
+                        path = ""
+                    }
                 })
+            },
+
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered()
+            },
+
+            sorting = {
+                priority_weight = 2,
+                comparators = {
+                    require("cmp-under-comparator").under,
+                    cmp.config.compare.offset,
+                    cmp.config.compare.exact,
+                    cmp.config.compare.score,
+                    cmp.config.compare.kind,
+                    cmp.config.compare.length,
+                    cmp.config.compare.order
+                }
             }
         })
+
     end
 }
