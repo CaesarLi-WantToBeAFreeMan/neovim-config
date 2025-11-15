796446625c021ff41d36205d0c1d5bcf04259e1c
CaesarLi-WantToBeAFreeMan <krbw0110010000@gmail.com>
2025-11-15 22:54:23 +0800

    remove falsh plugin

--- lua/plugin/editor.lua
+++ lua/plugin/editor.lua
@@ -1,127 +1,62 @@
-return{
+return {
     --search/replace in multiple files
     {
         "MagicDuck/grug-far.nvim",
         event = "VeryLazy",
         opts = {
-            headerMaxWidth = 90
+            headerMaxWidth = 90,
         },
         cmd = {
             "GrugFar",
-            "GrugFarWithin"
+            "GrugFarWithin",
         },
         keys = {
             {
-                "<leader>sr",           --search and replace
+                "<leader>sr", --search and replace
                 function()
                     local grug, ext = require("grug-far"), vim.bo.buftype == "" and vim.fn.expand("%:e")
                     grug.open({
                         transient = true,
                         prefiles = {
-                            filesFilter = ext and ext ~= "" and "*." .. ext or nil
-                        }
+                            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+                        },
                     })
                 end,
-                mode = {"n", "x"},
-                desc = "search and replace in multiple files"
-            }
-        }
-    },
-    --enhance search functionality
-    {
-        "folke/flash.nvim",
-        event = "VeryLazy",
-        vscode = true,
-        opts = {},
-        keys = {
-            {
-                "s",
-                mode = {"n", "x", "o"},
-                function()
-                    require("flash").jump()
-                end,
-                desc = "flash"
+                mode = { "n", "x" },
+                desc = "search and replace in multiple files",
             },
-            {
-                "S",
-                mode = {"n", "o", "x"},
-                function()
-                    require("flash").treesitter()
-                end,
-                desc = "flash treesitter"
-            },
-            {
-                "r",
-                mode = "o",
-                function()
-                    require("flash").remote()
-                end,
-                desc = "remote flash"
-            },
-            {
-                "R",
-                mode = {"o", "x"},
-                function()
-                    require("flash").treesitter_search()
-                end,
-                desc = "treesitter search"
-            },
-            {
-                "<C-s>",
-                mode = {"c"},
-                function()
-                    require("flash").toggle()
-                end,
-                desc = "toggle flash search"
-            },
-            {
-                "<C-Space>",
-                mode = {"n", "o", "x"},
-                function()
-                    require("flash").treesitter({
-                        actions = {
-                            ["<C-n>"] = "next",
-                            ["<C-p>"] = "prev"
-                        }
-                    })
-                end
-            }
-        }
+        },
     },
     --find and list all of the TODO
     {
         "folke/todo-comments.nvim",
         cmd = {
             "TodoTrouble",
-            "TodoTelescope"
+            "TodoTelescope",
         },
         event = "VeryLazy",
         opts = {},
         keys = {
             {
                 "[t",
-                function()
-                    require("todo-comments").jump_prev()
-                end,
-                desc = "previous TODO comment"
+                function() require("todo-comments").jump_prev() end,
+                desc = "previous TODO comment",
             },
             {
                 "]t",
-                function()
-                    require("todo-comments").jump_next()
-                end,
-                desc = "next TODO comment"
+                function() require("todo-comments").jump_next() end,
+                desc = "next TODO comment",
             },
             {
                 "<leader>tt",
                 "<cmd>TodoTelescope<CR>",
-                desc = "list TODO comments via telescope"
+                desc = "list TODO comments via telescope",
             },
             {
                 "<leader>tT",
                 "<cmd>Trouble todo toggle<CR>",
-                desc = "list TODO comments via Trouble"
-            }
-        }
-    }
+                desc = "list TODO comments via Trouble",
+            },
+        },
+    },
 }
